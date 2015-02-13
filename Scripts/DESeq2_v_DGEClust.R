660236c861909312a1a6e530fe7e5f365d580a4a
hobrien <heath.obrien@gmail.com>
2015-02-13 14:55:44 +0000

    changed output to volcano plot and list of sig genes DE by >2 fold
    I'm now comparing the regularised fold differences from DEseq2 vs. the adjusted p-values
    from DGEclust.
    
    The overlap is genes with p<0.01 and more than 2 fold expression diff
    
    Fold differences are "regularised" to downweight outliers

--- Scripts/DESeq2_v_DGEClust.R
+++ Scripts/DESeq2_v_DGEClust.R
@@ -1,101 +1,56 @@
 library(ggplot2)
 library(plyr)
 library(DESeq2)
-library(VennDiagram)
+library(scales)
+library(RColorBrewer)
+require(grid)
 
+fte_theme <- function() {
+    
+    # Generate the colors for the chart procedurally with RColorBrewer
+    palette <- brewer.pal("Greys", n=9)
+    color.background = palette[2]
+    color.grid.major = palette[3]
+    color.axis.text = palette[6]
+    color.axis.title = palette[7]
+    color.title = palette[9]
+    
+    # Begin construction of chart
+    theme_bw(base_size=9) +
+        
+        # Set the entire chart region to a light gray color
+        theme(panel.background=element_rect(fill=color.background, color=color.background)) +
+        theme(plot.background=element_rect(fill=color.background, color=color.background)) +
+        theme(panel.border=element_rect(color=color.background)) +
+        
+        # Format the grid
+        theme(panel.grid.major=element_line(color=color.grid.major,size=.25)) +
+        theme(panel.grid.minor=element_blank()) +
+        theme(axis.ticks=element_blank()) +
+        
+        # Format the legend, but hide by default
+        theme(legend.position="none") +
+        theme(legend.background = element_rect(fill=color.background)) +
+        theme(legend.text = element_text(size=7,color=color.axis.title)) +
+        
+        # Set title and axis labels, and format these and tick marks
+        theme(plot.title=element_text(color=color.title, size=10, vjust=1.25)) +
+        theme(axis.text.x=element_text(size=9,color=color.axis.text)) +
+        theme(axis.text.y=element_text(size=9,color=color.axis.text)) +
+        theme(axis.title.x=element_text(size=10,color=color.axis.title, vjust=0)) +
+        theme(axis.title.y=element_text(size=10,color=color.axis.title, vjust=1.25)) +
+        
+        # Plot margins
+        theme(plot.margin = unit(c(0.35, 0.2, 0.3, 0.35), "cm"))
+}
+reverselog_trans <- function(base = exp(1)) {
+     trans <- function(x) -log(x, base)
+     inv <- function(x) base^(-x)
+     trans_new(paste0("reverselog-", format(base)), trans, inv, 
+               log_breaks(base = base), 
+               domain = c(1e-100, Inf))
+}
 
-MergeResults<-function(DEseq, DGEclust, name){
-  #rename columns and add info about species and comparison
-  #row.names(DEseq)<-DEseq$id
-  DEseq["samples"]<-name
-  DEseq<-rename(DEseq, c("pvalue" = "DEseq_pval", "padj"="DEseq_padj"))
-  
-  #rename DGEclust columns, if necessary
-  #DGEclust<-rename(DGEclust, c("Posteriors" = "pval", "FDR" = "padj"))
-  DEseq$"DGEclust_pval"<-DGEclust[,"pval"][match(row.names(DEseq), row.names(DGEclust))]
-  DEseq$"DGEclust_padj"<-DGEclust[,"padj"][match(row.names(DEseq), row.names(DGEclust))]
-  
-  
-  #remove missing values that will cause an error in the following step
-  DEseq<-na.omit(DEseq)
-  
-  #this will replace extreme values with zero, which makes the plot nicer
-  if (nrow(DEseq[DEseq$DEseq_padj < 1e-5, ]["DEseq_padj"]) > 0 ) {
-    DEseq[DEseq$DEseq_padj < 1e-5, ]["DEseq_padj"] <- 0
-  }
-  return(as.data.frame(DEseq))
-}
-scatterplot<-function(pvalues, DEseq_cutoff, DGEclust_cutoff, name) {
-  #make scatterplot of adjusted p-values and Venn Diagram of overlap
-  sp = ggplot(pvalues, aes(x=DEseq_padj, y=DGEclust_padj)) +
-    geom_point(size=1.2) +
-    scale_x_log10(limits=c(1e-5,1), breaks=c(1e-5, 1e-4, 1e-3, 0.01, 0.1, 1)) +
-    scale_y_log10(limits=c(1e-5,1), breaks=c(1e-5, 1e-4, 1e-3, 0.01, 0.1, 1)) +
-    geom_hline(yintercept=DGEclust_cutoff, size=.2) +
-    geom_vline(xintercept=DEseq_cutoff, size=.2) +
-    annotate("text", x=1e-5, y=1, hjust=0, label=sprintf("r = %.3f", cor(results$DEseq_padj,results$DGEclust_padj))) +
-    annotate("text", hjust=0, x=1e-5, y=.7, label=sprintf("p = %.5f", cor.test(pvalues$DEseq_padj,pvalues$DGEclust_padj)$p.value)) +
-    theme_bw() +
-    ggtitle(name)
-  #geom_point(data=results[results$id == 'cluster_19641',], colour='red')
-  return(sp)
-}
-vennplot <-function(pvalues, DEseq_cutoff, DGEclust_cutoff) {
-  if ( nrow(pvalues[pvalues$DGEclust_padj < DGEclust_cutoff, ]) > 0 && nrow(pvalues[pvalues$DEseq_padj < DEseq_cutoff, ]) > 0 ) {
-    if ( nrow(pvalues[pvalues$DGEclust_padj < DGEclust_cutoff, ]) < nrow(pvalues[pvalues$DEseq_padj < DEseq_cutoff, ]) ) {
-      draw.pairwise.venn(area1=nrow(pvalues[results$DGEclust_padj < DGEclust_cutoff, ]), 
-                         area2=nrow(pvalues[pvalues$DEseq_padj < DEseq_cutoff, ]), 
-                         cross.area=nrow(pvalues[pvalues$DEseq_padj < DEseq_cutoff & pvalues$DGEclust_padj < DGEclust_cutoff, ]), 
-                         fill=c('blue', 'red'), 
-                         cat.col = c('blue', 'red'),
-                         category=c("DEseq", "DGEclust"), 
-                         lty = 'blank',
-                         cex = 2,
-                         cat.cex = 1.75,
-                         margin=0.2,
-                         fontfamily='sans',
-                         cat.fontfamily='sans')
-    }
-    else {
-      draw.pairwise.venn(area1=nrow(pvalues[pvalues$DGEclust_padj < DGEclust_cutoff, ]),
-                         area2=nrow(pvalues[pvalues$DEseq_padj < DEseq_cutoff, ]), 
-                         cross.area=nrow(pvalues[pvalues$DEseq_padj < DEseq_cutoff & pvalues$DGEclust_padj < DGEclust_cutoff, ]), 
-                         fill=c('red', 'blue'), 
-                         cat.col = c('red', 'blue'),
-                         category=c("DGEclust", "DEseq"), 
-                         lty = 'blank',
-                         cex = 2,
-                         cat.cex = 1.75,
-                         margin=0.2,
-                         fontfamily='sans',
-                         cat.fontfamily='sans')
-    }
-  }
-  else if (nrow(pvalues[pvalues$DGEclust_padj < DGEclust_cutoff, ]) > 0) {
-    draw.single.venn(area=nrow(pvalues[pvalues$DGEclust_padj < DGEclust_cutoff, ]), 
-                     fill=c('red'), 
-                     cat.col = c('red'),
-                     category=c("DGEclust"), 
-                     lty = 'blank',
-                     cex = 2,
-                     cat.cex = 1.75,
-                     margin=0.2,
-                     fontfamily='sans',
-                     cat.fontfamily='sans')
-  }
-  else if (nrow(pvalues[pvalues$DEseq_padj < DEseq_cutoff, ]) > 0) {
-    draw.single.venn(area=nrow(pvalues[pvalues$DEseq_padj < DEseq_cutoff, ]), 
-                     fill=c('blue'), 
-                     cat.col = c('blue'),
-                     category=c("DEseq"), 
-                     lty = 'blank',
-                     cex = 2,
-                     cat.cex = 1.75,
-                     margin=0.2,
-                     fontfamily='sans',
-                     cat.fontfamily='sans')
-  }
-}
 
 args <- commandArgs(trailingOnly = TRUE)
 DGEclust_file <- args[1] #"Bioinformatics/Selaginella_DGE/DGEclust/KRAUS_12.txt"
@@ -103,8 +58,7 @@ DGEclust_file <- args[1] #"Bioinformatics/Selaginella_DGE/DGEclust/KRAUS_12.txt"
 CountTable =  read.table( file=args[2] , header=TRUE, row.names=1 )
 #DGEclust_cutoff <- as.numeric(0.01)
 DGEclust_cutoff <- as.numeric(args[3])
-DEseq_cutoff<- DGEclust_cutoff
-plotfile<-paste(strsplit(DGEclust_file, '\\.')[[1]][1], DGEclust_cutoff, 'pvals2.pdf', sep='_') 
+plotfile<-paste(strsplit(DGEclust_file, '\\.')[[1]][1], 'volcano.pdf', sep='_') 
 textfile<-paste(strsplit(DGEclust_file, '\\.')[[1]][1], DGEclust_cutoff, 'overlap2.txt', sep='_') 
 species_regex<-gregexpr('(KRAUS)|(MOEL)|(UNC)|(WILD)', DGEclust_file)
 regex_start <- species_regex[[1]][1]
@@ -119,15 +73,23 @@ DEseq_data<-DESeqDataSetFromMatrix(countData = CountTable,
                                   colData = Setup,
                                   design = ~ condition)
 DEseq_data <- DESeq(DEseq_data)
-DEseq_results <- results(DEseq_data)
+rld <- rlogTransformation( DEseq_data )
+results <- data.frame(
+    assay(rld), 
+    avgLogExpr = ( assay(rld)[,2] + assay(rld)[,1] ) / 2,
+    rLogFC = assay(rld)[,2] - assay(rld)[,1] )
 DGEclust<-read.table(file=DGEclust_file, header=T)
-results<-MergeResults(DEseq_results, DGEclust, name)
-#write list of overlaps to textfile
-write.table(results[results$DEseq_padj < DEseq_cutoff & results$DGEclust_padj < DGEclust_cutoff, ], textfile)
+results$"DGEclust_padj"<-DGEclust[,"padj"][match(row.names(results), row.names(DGEclust))]
 
+write.table(results[results$DGEclust_padj < DGEclust_cutoff & abs(results$rLogFC)>1, ], textfile)
+title <- sprintf("Volcano plot for %s vs %s", sample1, sample2)
 pdf(plotfile)
-print(scatterplot(results, DEseq_cutoff, DGEclust_cutoff, name))
-print(vennplot(results, DEseq_cutoff, DGEclust_cutoff))
+ggplot(results, aes(x=rLogFC, y=DGEclust_padj)) +
+	geom_point(alpha=0.1, size=2, colour="dark blue") +
+	geom_point(size=2, data=subset(results, DGEclust_padj < 0.01 & abs(rLogFC)>1), aes(colour='red')) +
+	fte_theme() +
+	scale_y_continuous(trans = reverselog_trans(10)) +
+	labs(title=title, x="fold change (log2)", y="p-values")
 dev.off()
 
 
