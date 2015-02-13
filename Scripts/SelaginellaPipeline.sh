0d240b36da52d453e29ffb15e163f5230a216385
hobrien <heath.obrien@gmail.com>
2015-02-13 16:30:43 +0000

    Changed from DEseq to DEseq2

--- Scripts/SelaginellaPipeline.sh
+++ Scripts/SelaginellaPipeline.sh
@@ -87,7 +87,7 @@ do
       if ! test $species = 'MOEL' || ! test $leaf2 = 3
       then
         pvals -t0 1000 -tend 10000 -g1 $leaf1 -g2 $leaf2 -o $BASEDIR/DGEclust/${species}_$(($leaf1+1))$(($leaf2+1)).txt -i $BASEDIR/DGEclust/$species
-        Rscript $BASEDIR/Scripts/DESeq_v_DGEClust.R $BASEDIR/DGEclust/${species}_$(($leaf1+1))$(($leaf2+1)).txt $BASEDIR/DGEclust/${species}counts.txt 0.001
+        Rscript $BASEDIR/Scripts/DESeq2_v_DGEClust.R $BASEDIR/DGEclust/${species}_$(($leaf1+1))$(($leaf2+1)).txt $BASEDIR/DGEclust/${species}counts.txt 0.01
       fi
     done
   done
