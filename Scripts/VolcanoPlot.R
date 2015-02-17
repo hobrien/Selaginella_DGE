e5844409307c1df50ad8aee397c9b675589084ae
hobrien <heath.obrien@gmail.com>
2015-02-17 10:02:24 +0000

    fixed formatting of output

--- Scripts/VolcanoPlot.R
+++ Scripts/VolcanoPlot.R
@@ -57,6 +57,7 @@ reverselog_trans <- function(base = exp(1)) {
 #Get data from DB
 con<-dbConnect(RMySQL::MySQL(), user='root', password='', host='localhost', dbname='SelaginellaGenomics')
 Expression <- dbReadTable(conn = con,name = 'Expression')
+#Expression <- dbGetQuery(conn = con, statement = "SELECT * FROM Expression WHERE sample1 = 'KRAUS1' AND sample2 = 'KRAUS2'")
 DEgenes <- dbGetQuery(conn = con, statement = "SELECT * FROM Expression WHERE (rLogFC > 1 OR rLogFC < -1) AND DGEclust_padj < 0.01")
 
 #need a couple of the formatting elements to draw lines
@@ -69,13 +70,14 @@ args <- commandArgs(trailingOnly = TRUE)
 plotfile <- args[1]
 png(plotfile, bg='transparent', res = 300, width=15, height=15, units='cm')
 ggplot(Expression, aes(x=rLogFC, y=DGEclust_padj, colour=speciesID)) +
-    geom_hline(yintercept=.01, colour="dark grey", size=1) +
+    geom_hline(yintercept=.01, colour="dark grey") +
     geom_segment(aes(x=-1, xend=1, y=.01, yend=.01), colour=color.background, size=1) +
     geom_segment(aes(x=-1, xend=1, y=.01, yend=.01), colour=color.grid.major, size=.1) +
     geom_segment(aes(x=-1, xend=-1, y=.01, yend=.00001), colour="dark grey") +
     geom_segment(aes(x=1, xend=1, y=.01, yend=.00001), colour="dark grey") +
+    geom_vline(xintercept=0, colour=color.grid.major, size=.1) +
     geom_point(alpha=0.2, size=1) +
-    geom_point(data = DEgenes, size=2, aes(colour=speciesID)) +
+    geom_point(data = DEgenes, size=1, aes(colour=speciesID)) +
     scale_y_continuous(trans = reverselog_trans(10), breaks=c(.1,.01,.001, 0.0001, 0.00001), labels=comma) +
     scale_x_continuous(breaks=c(-3, -2, -1, 0, 1, 2, 3)) +
     labs(x="fold change (log2)", y="p-values") +
