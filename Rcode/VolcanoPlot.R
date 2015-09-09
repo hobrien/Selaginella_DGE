4d98d0a6377587704e5c3c69e792de35176d708f
hobrien <heath.obrien@gmail.com>
2015-09-09 14:58:09 +0100

    refactored as a function so that it can be imported into a markdown file

--- Rcode/VolcanoPlot.R
+++ Rcode/VolcanoPlot.R
@@ -7,21 +7,21 @@ library(RMySQL)
 
 source("~/Documents/BTsync2/R/FormatGGplot.R")
 #Get data from DB
-con<-dbConnect(RMySQL::MySQL(), user='root', password='', host='localhost', dbname='SelaginellaGenomics')
-Expression <- dbReadTable(conn = con,name = 'Expression')
-#Expression <- dbGetQuery(conn = con, statement = "SELECT * FROM Expression WHERE sample1 = 'KRAUS1' AND sample2 = 'KRAUS2'")
-DEgenes <- dbGetQuery(conn = con, statement = "SELECT * FROM Expression WHERE (rLogFC > 1 OR rLogFC < -1) AND DGEclust_padj < 0.01")
+
+Volcanoplot <- function(species, sample1, sample2) {
+  con<-dbConnect(RMySQL::MySQL(), user='root', password='', host='localhost', dbname='SelaginellaGenomics')
+  Expression <- dbReadTable(conn = con,name = 'Expression')
+  #Expression <- dbGetQuery(conn = con, statement = "SELECT * FROM Expression WHERE sample1 = 'KRAUS1' AND sample2 = 'KRAUS2'")
+  DEgenes <- dbGetQuery(conn = con, statement = "SELECT * FROM Expression WHERE (rLogFC > 1 OR rLogFC < -1) AND FDR < 0.01")
 
 #need a couple of the formatting elements to draw lines
-palette <- brewer.pal("Greys", n=9)
-color.background = palette[2]
-color.grid.major = palette[3]
+  palette <- brewer.pal("Greys", n=9)
+  color.background <- palette[2]
+  color.grid.major <- palette[3]
 
 #make the plot!
-args <- commandArgs(trailingOnly = TRUE)
-plotfile <- args[1]
-png(plotfile, bg='transparent', res = 300, width=15, height=15, units='cm')
-ggplot(Expression, aes(x=rLogFC, y=DGEclust_padj, colour=speciesID)) +
+
+  plot<- ggplot(Expression, aes(x=rLogFC, y=FDR, colour=speciesID)) +
     geom_hline(yintercept=.01, colour="dark grey") +
     geom_segment(aes(x=-1, xend=1, y=.01, yend=.01), colour=color.background, size=1) +
     geom_segment(aes(x=-1, xend=1, y=.01, yend=.01), colour=color.grid.major, size=.1) +
@@ -36,4 +36,10 @@ ggplot(Expression, aes(x=rLogFC, y=DGEclust_padj, colour=speciesID)) +
     fte_theme() +
     theme(legend.position="right") +
     guides(colour = guide_legend(override.aes = list(size=3)))
+  plot
+}
+args <- commandArgs(trailingOnly = TRUE)
+plotfile <- args[1]
+png(plotfile, bg='transparent', res = 300, width=15, height=15, units='cm')
+print(Volcanoplot(ALL))
 dev.off()
